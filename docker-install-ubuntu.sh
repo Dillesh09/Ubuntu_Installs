ebfe603ab6f14e548e88b723fdd0902817a2ff3d
Dillesh <127320603+Dillesh09@users.noreply.github.com>
2023-09-06 18:48:35 +0530

    Update docker-install-ubuntu.sh
    updating docker file

--- docker-install-ubuntu.sh
+++ docker-install-ubuntu.sh
@@ -1,7 +1,11 @@
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
 apt-cache policy docker-ce
 sudo apt install docker-ce
 sudo systemctl status docker
+sudo usermod -aG docker ${USER}
+su - ${USER}
